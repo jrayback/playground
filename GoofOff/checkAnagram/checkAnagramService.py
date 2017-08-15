6d068fb26b01500262d51db4b3bdd22f100ca19e
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2017-08-15 10:59:51 -0600

    Change from Tornado to Flask

--- GoofOff/checkAnagram/checkAnagramService.py
+++ GoofOff/checkAnagram/checkAnagramService.py
@@ -1,23 +1,11 @@
-import tornado.ioloop
-import tornado.web
+from flask import Flask
 import checkAnagram
+
+app = Flask(__name__)
 
 string1 = 'iceman'
 string2 = 'cinema'
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-   		
-    	result = 'true' if checkAnagram.checkAnagram(string1,string2) else 'false'
-        self.write("<head></head><body><h1 style=\"color:green;\">" + result + "</h1></body>")
-
-
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-
-if __name__ == "__main__":
-    app = make_app()
-    app.listen(8898)
-    tornado.ioloop.IOLoop.current().start()
+@app.route("/")
+def check():
+	return str(checkAnagram.checkAnagram(string1,string2))
