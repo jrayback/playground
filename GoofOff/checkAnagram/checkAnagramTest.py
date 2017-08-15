84f246b9bbd164353a19ef6056bfa481089cccba
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2017-08-15 17:55:48 -0600

    Add test for case of empty string.

--- GoofOff/checkAnagram/checkAnagramTest.py
+++ GoofOff/checkAnagram/checkAnagramTest.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python
 
 import unittest
-import checkAnagram
+from checkAnagram import checkAnagram
 
 class TestCheckAnagram(unittest.TestCase) :
 
 	def test_simple(self) :
 		str1 = 'cinema'
 		str2 = 'iceman'
-		self.assertTrue(checkAnagram.checkAnagram(str1,str2))
+		self.assertTrue(checkAnagram(str1,str2))
 
 	def test_extra_letters(self) :
 		str1 = 'cinemaa'
 		str2 = 'iceman'
-		self.assertFalse(checkAnagram.checkAnagram(str1,str2))
+		self.assertFalse(checkAnagram(str1,str2))
 
 	def test_mixed_case(self) :
 		str1 = 'Cinema'
 		str2 = 'iceman'
-		self.assertTrue(checkAnagram.checkAnagram(str1,str2))
+		self.assertTrue(checkAnagram(str1,str2))
 
 	def test_special_chars(self) :
 		str1 = 'f$~$/\n#011'
 		str2 = 'iceman'
-		self.assertFalse(checkAnagram.checkAnagram(str1,str2))
+		self.assertFalse(checkAnagram(str1,str2))
+
+	def test_empty_string(self) :
+		str1 = ''
+		str2 = 'icemen'
+		self.assertFalse(checkAnagram(str1,str2))
 
 if __name__ == '__main__' :
 	unittest.main()
