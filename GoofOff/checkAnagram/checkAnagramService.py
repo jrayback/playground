ff60eb7624519a6d508c81590c095b8993f51447
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2017-08-15 17:32:34 -0600

    Make check anagram service into initial, RESTful service using Flask. Implement default GET

--- GoofOff/checkAnagram/checkAnagramService.py
+++ GoofOff/checkAnagram/checkAnagramService.py
@@ -1,11 +1,25 @@
-from flask import Flask
-import checkAnagram
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask.ext.jsonpify import jsonify
+from checkAnagram import checkAnagram
 
 app = Flask(__name__)
+api = Api(app)
 
-string1 = 'iceman'
-string2 = 'cinema'
+class CheckAnagram(Resource):
+	# Default values for 'GET'
+	string1 = 'iceman'
+	string2 = 'cinema'	
 
-@app.route("/")
-def check():
-	return str(checkAnagram.checkAnagram(string1,string2))
+	def get(self): # Define the 'GET' verb for RESTful API
+		result = {'First String': self.string1, 'Second String': self.string2, 'isAnagram': str(check(self.string1, self.string2))}
+		return jsonify(result)
+	
+def check(str1, str2):
+	return checkAnagram(str1, str2)
+
+api.add_resource(CheckAnagram, '/comparison')
+
+
+if __name__ == '__main__':
+	app.run(port='5002')
