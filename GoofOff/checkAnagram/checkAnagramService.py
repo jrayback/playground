74c83ff013b94170ffa9a462efe1c127ffffe2da
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2017-08-18 15:04:51 -0600

    Add error handling for bad requests

--- GoofOff/checkAnagram/checkAnagramService.py
+++ GoofOff/checkAnagram/checkAnagramService.py
@@ -1,30 +1,30 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
 from flask_jsonpify import jsonify
+from flask_api import status
 from checkAnagram import checkAnagram
 
 app = Flask(__name__)
-api = Api(app)
 
-class CheckAnagram(Resource):
-	
-	#TODO - need error handling in case either(?)/both(?) strings are missing from the query string
-
-	# Set up strings to compare
-	#string1 = request.args.get('first')
-	#string2 = request.args.get('second')
-
-	def get(self): # Define the 'GET' verb for RESTful API
-		result = {'First String': request.args.get('first'), 
-					'Second String': request.args.get('second'), 
-					'isAnagram': str(check(request.args.get('first'), request.args.get('second')))}
-		return jsonify(result)
-	
 def check(str1, str2):
 	return checkAnagram(str1, str2)
 
-api.add_resource(CheckAnagram, '/comparison')
-
+@app.route('/comparison', methods=['GET'])
+def check_anagram():
+	first_string = request.args.get('first')
+	second_string = request.args.get('second')
+	status_code = status.HTTP_400_BAD_REQUEST
+	
+	# As long as both values were passed in the query string, check the anagram and return 200
+	if first_string is not None and second_string is not None:
+		result = {'First String': first_string, 
+					'Second String': second_string, 
+					'isAnagram': str(check(first_string, second_string))}
+		status_code = status.HTTP_200_OK
+	# otherwise return a message in the response and a 400 code
+	else:
+		result = {'Error': 'Need values for first and second to be passed in the query string.'}
+	
+	return jsonify(result), status_code
 
 if __name__ == '__main__':
-	app.run(port='5002')
+	app.run(port='5003')
