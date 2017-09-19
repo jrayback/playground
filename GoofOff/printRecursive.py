b0d81e096f40c39f2a921a5656bd2fb6e0e8d613
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2017-09-19 13:56:23 +0200

    add comments using vim editor

--- GoofOff/printRecursive.py
+++ GoofOff/printRecursive.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
+# need the time package for sleep
 import time
 
+# recursive function to call
 def print_char(arr) :
 	print arr.pop(0)
 	time.sleep(1)
@@ -10,5 +12,8 @@ def print_char(arr) :
 	else :
 		return;
 
+# the array to use
 array=[1,2,3,4,5,6,7,8,9,10]
+
+#now call the function
 print_char(array)
