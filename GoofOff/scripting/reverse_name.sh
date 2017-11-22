d40100a3f73e3773ae3e821b0c234b773c1bb3e7
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2017-11-22 16:51:18 -0700

    Add reverse alphabetical script

---
+++ GoofOff/scripting/reverse_name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# echo the input string into tr (translate) to make it all lower case grep, matching only alphanumeric and printing one match per line, 
+#pipe to sort to alphabetize, 
+#then pipe to tr (tanslate) to strip out newlines which, is to say, reconstitute the newly sorted string, then print a newline for 
+#looks.
+echo $1 | grep -o [a-zA-Z0-9] | sort -fr | tr -d "\n"; printf "\n"
