bc7d74594f5d950a09e9b6aca0592c305d7c1d15
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2018-01-03 22:05:05 -0700

    update script

--- GoofOff/javascript/scripts/trains.js
+++ GoofOff/javascript/scripts/trains.js
@@ -1,1 +1,31 @@
-var myVar = 1
+// var trainsOperational = 8;
+
+// for(var trainNumber = 1; trainNumber <= trainsOperational; trainNumber++){
+//     console.log("Train #" + trainNumber + " is running.");
+// }
+
+// var userName;
+
+// while(typeof userName != "string"){
+//     while(typeof userName != "string" || !(confirm("Please confirm your name is " + userName))){
+//         userName = prompt("Please enter your name:","");
+//     }
+// }
+
+var eCount = function(){
+    var phrase = prompt("Which prase would you like to examine?");
+    if(typeof phrase != "string"){
+        return false;
+    } else {
+        var eCount = 0;
+        for(var i = 0; i < phrase.length; i++){
+            if(phrase.charAt(i) == 'e' || phrase.charAt(i) == 'E'){
+                eCount++;
+            }
+        }
+        alert("There are " + eCount + " Es in \"" + phrase + "\".");
+        return true;
+    }
+};
+
+eCount();
