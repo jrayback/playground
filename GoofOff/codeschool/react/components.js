6c806ebaa7f60a6954df69fdb8d12611b1be388b
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2018-01-17 11:11:46 -0700

    modify files. React stuff still not working. tis a problem for another day.

---
+++ GoofOff/codeschool/react/components.js
@@ -0,0 +1,9 @@
+class StoryBox extends React.Component {
+    render(){
+        return( <div>Story Box</div> );
+    }
+}
+
+ReactDom.render(
+    <StoryBox />, document.getElementById('story-app')
+);
