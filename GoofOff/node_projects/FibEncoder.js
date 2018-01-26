26e55195c0b2fe7c4ed0ca5fc29ffdd9785e5fc6
Jonathan Rayback <jonathan.rayback@motorolasolutions.com>
2018-01-26 15:18:50 -0700

    Finish modifying variable names to fit substitution cipher terminology

--- GoofOff/node_projects/FibEncoder.js
+++ GoofOff/node_projects/FibEncoder.js
@@ -11,38 +11,40 @@ function generateCharsFromInts (intArray) {
 // Ultimately I could abstract this class to use different substitution ciphers
 class FibEncoder {
   constructor () {
+    // This is couples to Fibonacci.
     this.math = new MyMath()
     this.plainUnits = Array.from('abcdefghijklmnopqrstuvwxyz')
     this.cipherUnits = generateCharsFromInts(this.math.fib(26)) // 26 is magic number...also we're tightly coupled to fibonacci
+    // End coupling to Fibonacci...
   }
-  encode (message) {
-    let encodedMessage = ''
-    let lowerMsg
+  encode (plainText) {
+    let cipherText = ''
+    let toLower
     try {
       // Try to make lower case, but if it's not an alphabetical character, it handles the error and does nothing
-      lowerMsg = message.toLowerCase()
+      toLower = plainText.toLowerCase()
     } catch (e) {
       console.log('Message must be alphabetical characters only.')
-      return encodedMessage
+      return cipherText
     }
     // strip out any spaces
-    let noSpaces = lowerMsg.replace(/\s/g, '')
+    let processedPlainText = toLower.replace(/\s/g, '')
     // make an array of the lowercase characters
-    let lowerCases = Array.from(noSpaces)
-    for (let char of lowerCases) {
+    let processedPlainTextArray = Array.from(processedPlainText)
+    for (let char of processedPlainTextArray) {
       let i = this.plainUnits.indexOf(char)
-      encodedMessage += this.cipherUnits[i]
+      cipherText += this.cipherUnits[i]
     }
-    return encodedMessage
+    return cipherText
   }
-  decode (message) {
-    let decodedMessage = ''
-    let keys = this._parseKeys(message, 6) // 6 is magic number, where to put it?
+  decode (cipherText) {
+    let plainText = ''
+    let keys = this._parseKeys(cipherText, 6) // 6 is magic number, where to put it?
     for (let key of keys) {
       let i = this.cipherUnits.indexOf(key)
-      decodedMessage += this.plainUnits[i]
+      plainText += this.plainUnits[i]
     }
-    return decodedMessage
+    return plainText
   }
   // I NEED TO BREAK THIS OUT INTO A SEPARATE PARSING STATIC CLASS WITH A parseFib() FUNCTION THEN SET THIS INSTANCE
   // OF THE DECODER TO USE THE FIB PARSER
