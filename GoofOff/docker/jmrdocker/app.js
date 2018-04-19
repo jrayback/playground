45379fe11612945da82abf47da4cbc98ae9b2ef2
Jonathan Rayback <jrayback@gmail.com>
2018-04-18 19:59:06 -0600

    initial commit of simple nodeJS http server containerized in Docker

---
+++ GoofOff/docker/jmrdocker/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use((request, response, next) => {
+    console.log(request.headers)
+    next()
+  })
+  
+  app.use((request, response, next) => {
+    request.chance = Math.random()
+    next()
+  })
+  
+  app.use((err, request, response, next) => {
+    // log the error, for now just console.log
+    console.log(err)
+    response.status(500).send('Something broke!')
+  })
+
+  app.get('/', (request, response) => {
+    response.json({
+      chance: request.chance
+    })
+  })
+  
+  app.listen(port)
